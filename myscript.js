1dbb11bcf053c26a646e3c0e9153a8d24072408b
viktoria Herzer <viktoria.herzer@gmail.com>
2017-12-25 20:05:20 +0100

    second commit, add in java and change heart

--- myscript.js
+++ myscript.js
@@ -8,28 +8,50 @@ var Persons = [
 	likes: 0 
 	}, 
 		{
-	name: 'John',
-	surname: 'Doe',
+	name: 'Kate',
+	surname: 'Bee',
+	favoritePerformer: ["Justin Timberlake", "Ed Sheeran", "Emma Watson"],
+	age: 22,
+	myPhoto: "img/image.1jpg",
+	likes: 0 
+	}, 
+		{
+	name: 'Megi',
+	surname: 'Smith',
 	favoritePerformer: ["Justin Timberlake", "Ed Sheeran", "Emma Watson"],
 	age: 25,
 	myPhoto: "img/image.1jpg",
 	likes: 0 
 	}, 
 		{
-	name: 'John',
-	surname: 'Doe',
+	name: 'Big',
+	surname: 'Shaq',
 	favoritePerformer: ["Justin Timberlake", "Ed Sheeran", "Emma Watson"],
-	age: 25,
-	myPhoto: "img/image.1jpg",
-	likes: 0 
-	}, 
-		{
-	name: 'John',
-	surname: 'Doe',
-	favoritePerformer: ["Justin Timberlake", "Ed Sheeran", "Emma Watson"],
-	age: 25,
+	age: 26,
 	myPhoto: "img/image.1jpg",
 	likes: 0 
 	}, 
 
 ];
+console.log(Persons);
+
+function likeFunction(){
+	document.getElementById("numberoflikes").innerHTML ="6";
+};
+
+document.getElementById("name1").innerHTML = Persons[0].name;
+document.getElementById("name2").innerHTML = Persons[1].name;
+document.getElementById("name3").innerHTML = Persons[2].name;
+document.getElementById("name4").innerHTML = Persons[3].name;
+document.getElementById("sname1").innerHTML = Persons[0].surname;
+document.getElementById("sname2").innerHTML = Persons[1].surname;
+document.getElementById("sname3").innerHTML = Persons[2].surname;
+document.getElementById("sname4").innerHTML = Persons[3].surname;
+document.getElementById("age1").innerHTML = Persons[0].age;
+document.getElementById("age2").innerHTML = Persons[1].age;
+document.getElementById("age3").innerHTML = Persons[2].age;
+document.getElementById("age4").innerHTML = Persons[3].age;
+
+
+
+
