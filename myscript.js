bbe78d1894df50ec371b0a1dd1dc0ca216c34c2e
viktoria Herzer <viktoria.herzer@gmail.com>
2017-12-25 16:00:41 +0100

    first commit

---
+++ myscript.js
@@ -0,0 +1,35 @@
+var Persons = [
+	{
+	name: 'John',
+	surname: 'Doe',
+	favoritePerformer: ["Justin Timberlake", "Ed Sheeran", "Emma Watson"],
+	age: 25,
+	myPhoto: "img/image.1jpg",
+	likes: 0 
+	}, 
+		{
+	name: 'John',
+	surname: 'Doe',
+	favoritePerformer: ["Justin Timberlake", "Ed Sheeran", "Emma Watson"],
+	age: 25,
+	myPhoto: "img/image.1jpg",
+	likes: 0 
+	}, 
+		{
+	name: 'John',
+	surname: 'Doe',
+	favoritePerformer: ["Justin Timberlake", "Ed Sheeran", "Emma Watson"],
+	age: 25,
+	myPhoto: "img/image.1jpg",
+	likes: 0 
+	}, 
+		{
+	name: 'John',
+	surname: 'Doe',
+	favoritePerformer: ["Justin Timberlake", "Ed Sheeran", "Emma Watson"],
+	age: 25,
+	myPhoto: "img/image.1jpg",
+	likes: 0 
+	}, 
+
+];
