24dd7423289125019f0bd45af6f9d2dd3809d7f8
viktoria Herzer <viktoria.herzer@gmail.com>
2017-12-28 02:01:08 +0100

    final commit

--- myscript.js
+++ myscript.js
@@ -35,9 +35,9 @@ var Persons = [
 ];
 console.log(Persons);
 
-function likeFunction(){
+/*function likeFunction(){
 	document.getElementById("numberoflikes").innerHTML ="6";
-};
+};*/
 
 document.getElementById("name1").innerHTML = Persons[0].name;
 document.getElementById("name2").innerHTML = Persons[1].name;
@@ -52,6 +52,58 @@ document.getElementById("age2").innerHTML = Persons[1].age;
 document.getElementById("age3").innerHTML = Persons[2].age;
 document.getElementById("age4").innerHTML = Persons[3].age;
 
+/* function likeFunction(){
+ 	like1++;
+ 	document.getElementById("numberoflikes").innerHTML = like1;
+ };
+*/
+/*function likeFunction(val) {
+    var qty = document.getElementById('numberoflikes').value;
+    var new_qty = parseInt(qty,10) + val;
+    
+    if (new_qty < 0) {
+        new_qty = 0;
+    }
+    
+    document.getElementById('numberoflikes').value = new_qty;
+    return new_qty;
+}*/
+/*function incrementValue()
+{
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
+}
+*/
+ /*var i = 0;
+    function buttonClick() {
+        document.getElementById('inc').value = ++i;
+    }	*/
+    var numlikes1 = 0;
 
+    function incrementValue1(val) {
+    numlikes1 +=1;
+    document.getElementById("numberoflikes1").innerHTML = numlikes1;
+    }
 
+     var numlikes2 = 0;
+
+    function incrementValue2(val) {
+    numlikes2 +=1;
+    document.getElementById("numberoflikes2").innerHTML = numlikes2;
+    }
+    
+     var numlikes3 = 0;
+
+    function incrementValue3(val) {
+    numlikes3 +=1;
+    document.getElementById("numberoflikes3").innerHTML = numlikes3;
+    }
+     var numlikes4 = 0;
+
+    function incrementValue4(val) {
+    numlikes4 +=1;
+    document.getElementById("numberoflikes4").innerHTML = numlikes4;
+    }
 
